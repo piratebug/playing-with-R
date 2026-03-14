4e67e96a4dc0cd8f571ef99e9a2cabc2d36976dc
Alex Clasen <lackadaisicalladybug@gmail.com>
2026-03-14 12:34:36 -0700

    Adding custom colors to nodes

--- myFirstSankeyDiagram.R
+++ myFirstSankeyDiagram.R
@@ -4,13 +4,13 @@ library(dplyr)
 
 # A connection data frame is a list of flows with intensity for each flow
 links <- data.frame(
-  source=c("Indeed","Indeed", "ZipRecruiter", "ZipRecruiter", "LinkedIn", "LinkedIn", "Government Sites", 
-           "Government Sites", "Company Sites", "Company Sites", "GIS Jobs", "GIS Jobs", "Other Jobs", 
-           "Other Jobs", "Cover Letter", "Cover Letter", "Cover Letter", "No Cover Letter", "No Cover Letter",
-           "No Cover Letter"), 
-  target=c("GIS Jobs","Other Jobs", "GIS Jobs", "Other Jobs", "GIS Jobs", "Other Jobs", "GIS Jobs", 
-           "Other Jobs", "GIS Jobs", "Other Jobs", "Cover Letter", "No Cover Letter", "Cover Letter", 
-           "No Cover Letter", "Not Selected", "Ghosted", "Pending", "Not Selected", "Ghosted", "Pending"), 
+  source=c("Indeed","Indeed", "ZipRecruiter", "ZipRecruiter", "LinkedIn", "LinkedIn", "Government_Sites", 
+           "Government_Sites", "Company_Sites", "Company_Sites", "GIS_Jobs", "GIS_Jobs", "Other_Jobs", 
+           "Other_Jobs", "Cover_Letter", "Cover_Letter", "Cover_Letter", "No_Cover", "No_Cover",
+           "No_Cover"), 
+  target=c("GIS_Jobs","Other_Jobs", "GIS_Jobs", "Other_Jobs", "GIS_Jobs", "Other_Jobs", "GIS_Jobs", 
+           "Other_Jobs", "GIS_Jobs", "Other_Jobs", "Cover_Letter", "No_Cover", "Cover_Letter", 
+           "No_Cover", "Not_Selected", "Ghosted", "Pending", "Not_Selected", "Ghosted", "Pending"), 
   value=c(4,21,2,3,11,5,9,2,26,17,15,37,9,39,18,3,3,27,27,22)
 )
 
@@ -24,10 +24,16 @@ nodes <- data.frame(
 links$IDsource <- match(links$source, nodes$name)-1 
 links$IDtarget <- match(links$target, nodes$name)-1
 
+# Adding a color scale, which does not appreciate spaces between words
+my_color <- 'd3.scaleOrdinal() .domain(["Indeed", "ZipRecruiter","LinkedIn", "Government_Sites", 
+"Company_Sites", "GIS_Jobs", "Other_Jobs", "Cover_Letter", "No_Cover", "Not_Selected", "Ghosted",
+"Pending"]) .range(["#00008B", "#32CD32", "#4169E1", "#8B4513" , "#FF8C00", "#228B22", "#98FB98", 
+"#70E0D0", "#E0FFFF", "#FF6347", "#DCDCDC", "#F0E68C"])'
+
 # Make the Network
 p <- sankeyNetwork(Links = links, Nodes = nodes,
                    Source = "IDsource", Target = "IDtarget",
-                   Value = "value", NodeID = "name", 
+                   Value = "value", NodeID = "name", colourScale = my_color,
                    sinksRight=FALSE)
 p
 
